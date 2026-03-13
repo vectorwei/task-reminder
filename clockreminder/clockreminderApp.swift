cfb064b8ce33eb77cb32c2ae1515c6bdd2292f56
Jiawei <jiaweix1@uchicago.edu>
2026-03-13 11:15:35 -0700

    Initial Commit

---
+++ clockreminder/clockreminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockreminderApp.swift
+//  clockreminder
+//
+//  Created by Jiawei Xiong on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct clockreminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clockreminderTests/clockreminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  clockreminderTests.swift
+//  clockreminderTests
+//
+//  Created by Jiawei Xiong on 3/13/26.
+//
+
+import Testing
+@testable import clockreminder
+
+struct clockreminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
